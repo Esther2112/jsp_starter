8b61b5361723a8d9820c9c216acc496e995b7fa7
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-04-14 11:42:09 +0900

    servlet, jsp

--- src/main/java/com/jsp/chapter01/servlet/HelloServlet.java
+++ src/main/java/com/jsp/chapter01/servlet/HelloServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
 
 //WAS에게 해당 서블릿을 언제 사용할지 알려주는 코드
 @WebServlet("/hello")
@@ -26,6 +28,54 @@ public class HelloServlet extends HttpServlet {
     protected void service(
             HttpServletRequest req, HttpServletResponse resp
     ) throws ServletException, IOException {
+
         System.out.println("무슨일을 해볼깡");
+
+        //요청 방식
+        String method = req.getMethod();
+        //요청 URI
+        String uri = req.getRequestURI();
+        //요청 파라미터
+        String queryString = req.getQueryString();
+        //헤더 정보 읽기
+        String cc = req.getHeader("Cache-Control");
+
+        System.out.println("method = " + method);
+        System.out.println("uri = " + uri);
+        System.out.println("queryString = " + queryString);
+        System.out.println("cc = " + cc);
+
+        //쿼리 스트링 파라미터 값 가져오기
+        String name = req.getParameter("name");
+        String age = req.getParameter("age");
+
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+
+        //h1태그로 xxx님의 출생년도는 xxxx년입니다.
+        //핵심로직: 출생년도 구하기
+        int birthYear = LocalDate.now().getYear() - Integer.parseInt(age) + 1;
+        System.out.println("birthYear = " + birthYear);
+
+        //응답 메세지 생성
+        //펜을 가져와야됨
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter w = resp.getWriter();
+
+        w.write("<html>");
+        w.write("<head>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("   <h1>");
+        w.write(String.format("%s님은 %d년생입니다.", name, birthYear));
+        w.write("   </h1>");
+        w.write("</body>");
+        w.write("</html>");
+
+        w.flush(); //버퍼 비우기
+        w.close(); //펜 객체 해제
     }
 }
